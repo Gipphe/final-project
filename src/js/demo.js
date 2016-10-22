b9da81f5f1828b61b112fdb20f59aae5193c0626
Victor Nascimento Bakke <gipphe@gmail.com>
2016-10-23 01:34:28 +0200

    Remodeled filter to optionally accept exclusions before the predicate
    Added meaningful comments.

--- src/js/demo.js
+++ src/js/demo.js
@@ -19,11 +19,15 @@
 	// Gender input control
 	var genderInput = document.getElementById('gender-input');
 	var genderList = document.getElementById('gender-list');
-	var filter = function(list, predicate, exclusions) {
+	var filter = function(list, ex, predicate) {
+		if (typeof ex === 'function') {
+			predicate = ex;
+			ex = null;
+		}
 		var res = [];
 		for (var i = 0, len = list.length; i < len; i += 1) {
 			var item = list[i];
-			if (exclusions.includes(item.innerHTML)) {
+			if (ex && ex.includes(item.innerHTML)) {
 				res.push(item);
 			} else if (predicate(item)) {
 				res.push(item);
@@ -54,24 +58,35 @@
 		} else {
 			showList();
 		}
+		// Array of genders to exclude from filtering
+		var exclusion = ['Custom...'];
+		// List of gender LI HTMLElements
 		var list = genderList.children;
-		var showThese = filter(list, function(val) {
+		// bind filter with repeated variables
+		var filterList = filter.bind(null, list, exclusions);
+		// Filter out elements to display
+		var showThese = filterList(function(val) {
 			var html = val.innerHTML.toLowerCase();
 			var inputValue = genderInput.value.toLowerCase();
 			return html.includes(inputValue);
-		}, ['Custom...']);
-		var hideThese = filter(list, function(val) {
+		});
+		// Filter out elements to hide
+		var hideThese = filter(function(val) {
 			var html = val.innerHTML.toLowerCase();
 			var inputValue = genderInput.value.toLowerCase();
 			return !html.includes(inputValue);
-		}, ['Custom...']);
+		});
+		// Set display of hidden elements to 'none'
 		hideThese.forEach(function(el) {
 			setDisplay(el, 'none');
 		});
+		// Set display of shown elements to 'list-item'
 		showThese.forEach(function(el) {
 			setDisplay(el, 'list-item');
 		});
 	});
+	// Prevent the click event from bubbling up to #main, hiding the gender list
 	genderInput.addEventListener('click', function(e) { e.stopPropagation(); });
+	// Hide the gender list if a click event bubbles up to #main
 	document.getElementById('main').addEventListener('click', hideList);
 }());
