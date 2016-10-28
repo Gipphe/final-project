21647fbe3d778216b81833c9a8f0e59cfaa8dcb1
Victor Nascimento Bakke <gipphe@gmail.com>
2016-10-28 13:46:17 +0200

    Added language changing functionality to main script

---
+++ js/index.js
@@ -0,0 +1,17 @@
+(function() {
+	var en = document.getElementById('en');
+	var no = document.getElementById('no');
+	no.addEventListener('click', function(){
+		this.classList.add('selected');	
+		en.classList.remove('selected');
+	});
+
+	en.addEventListener('click', function() {
+		this.classList.add('selected');
+		no.classList.remove('selected');
+	});
+	window.changeLang = function(lang) {
+		if (typeof lang !== 'string' || lang.length !== 2) return;
+		document.documentElement.setAttribute('lang', lang);
+	};
+}());
