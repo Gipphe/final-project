1f020e8c16a7430313752aaad25749e3a3de1127
Victor Nascimento Bakke <gipphe@gmail.com>
2016-11-28 01:08:25 +0100

    Finalized title section

--- src/js/demo/title.js
+++ src/js/demo/title.js
@@ -3,6 +3,7 @@
 
 	var genderInput = document.getElementById('gender-input');
 	var genderList = document.getElementById('gender-list');
+	var compose = window.utils.compose;
 
 	var filter = function(list, ex, predicate) {
 		if (typeof ex === 'function') {
@@ -25,56 +26,87 @@
 		el.style.display = val;
 	};
 	var setListDisplay = function(val) {
-		setDisplay(genderList, val);
+		var trueVal = val === 'hide' ? '' : 'visible';
+		genderList.style.visibility = trueVal;
+	};
+	var hideListEl = function(el) {
+		setDisplay(el, 'none');
+	};
+	var showListEl = function(el) {
+		setDisplay(el, '');
+	};
+	var hideList = function() {
+		setListDisplay('hide');
+	};
+	var showList = function() {
+		setListDisplay('show');
 	};
 
-	var hideList = function(e) {
+	var halt = function(e) {
+		if (!e.stopPropagation) {
+			return e;
+		}
 		e.stopPropagation();
-		setListDisplay('none');
+		return e;
+	};
+	var setInputValue = function(e) {
+		genderInput.value = e.target.innerHTML;
+		return e;
+	};
+	var resetSelected = function() {
 		var children = genderList.children;
 		for (var i = 0; i < children.length; i += 1) {
 			var child = children[i];
 			child.classList.remove('selected');
 		}
 	};
-	var showList = function(e) {
-		e.stopPropagation();
-		setListDisplay('inline-block');
-	};
-	var getSelected = function() {
+	var getSelected = function(e) {
 		var children = genderList.children;
 		for (var i = 0, len = children.length; i < len; i += 1) {
 			var child = children[i];
 			if (child.classList.contains('selected')) {
-				return child;
+				return [e, child];
 			}
 		}
+		return [e];
 	};
-	genderList.addEventListener('click', function(e) {
-		e.stopPropagation();
-		// Copy list item value to input field
-		genderInput.value = e.target.innerHTML;
-		hideList();
-	});
-
-	genderList.addEventListener('hover', function(e) {
-		e.stopPropagation();
-		var selected = getSelected();
+	var checkSelected = function(container) {
+		var e = container[0];
+		var selected = container[1];
 		if (!selected) {
-			genderList.children[0].classList.add('selected');
+			selected = genderList.children[0];
+			selected.classList.add('selected');
 		}
+		return [e, selected];
+	};
+	var setSelected = function(container) {
+		var e = container[0];
+		var selected = container[1];
 		selected.classList.remove('selected');
-		e.target.claassList.add('selected');
-	});
+		e.target.classList.add('selected');
+		return [e, selected];
+	};
+	var setTitle = function() {
+		window.GenderAll.title = genderInput.value;
+	};
 
-	genderInput.addEventListener('click', showList, true);
-	genderInput.addEventListener('focusin', showList);
-	genderInput.addEventListener('input', function(e) {
-		e.stopPropagation();
+	var genderListClickHandle = compose(halt, setInputValue, hideList, resetSelected, setTitle);
+	var genderListHoverHandle = compose(halt, getSelected, checkSelected, setSelected);
+	genderList.addEventListener('click', genderListClickHandle);
+	genderList.addEventListener('mouseenter', genderListHoverHandle);
+
+	var showHideList = function() {
 		if (!genderInput.value) {
 			hideList();
+			return false;
 		} else {
 			showList();
+			return true;
+		}
+	};
+	var filterListItems = function(doRun) {
+		if (!doRun) {
+			return;
 		}
 		var exclusions = ['Custom...'];
 		var list = genderList.children;
@@ -90,12 +122,18 @@
 			return !html.includes(inputValue);
 		});
 		hideThese.forEach(function(el) {
-			setDisplay(el, 'none');
+			hideListEl(el);
 		});
 		showThese.forEach(function(el) {
-			setDisplay(el, 'list-item');
+			showListEl(el);
 		});
-	});
+	};
+	var genderInputClickHandle = compose(halt, showList);
+	var genderInputInputHandle = compose(halt, showHideList, filterListItems, setTitle);
+	genderInput.addEventListener('click', genderInputClickHandle);
+	genderInput.addEventListener('focusin', genderInputClickHandle);
+	genderInput.addEventListener('input', genderInputInputHandle);
+
 	var getFirstVisibleChild = function() {
 		var children = genderList.children;
 		for (var i = 0, len = children.length; i < len; i += 1) {
@@ -155,5 +193,5 @@
 	// Prevent the click event from bubbling up to #main, hiding the gender list
 	genderInput.addEventListener('click', function(e) { e.stopPropagation(); });
 	// Hide the gender list if a click event bubbles up to #main
-	document.getElementById('main').addEventListener('click', hideList);
+	document.getElementById('title').addEventListener('click', hideList);
 }());
