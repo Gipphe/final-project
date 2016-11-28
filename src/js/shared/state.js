7604a162ff8435814ab701a5f778f9c29d9c24ac
Victor Nascimento Bakke <gipphe@gmail.com>
2016-11-28 01:11:51 +0100

    Atomicized getNextState for State

--- src/js/shared/state.js
+++ src/js/shared/state.js
@@ -7,7 +7,7 @@
 			});
 			return els;
 		};
-		var getIndexByProperty = function(arr, prop, val) {
+		var indexOfWith = function(arr, prop, val) {
 			for (var i = 0, len = arr.length; i < len; i += 1) {
 				if (arr[i][prop] === val) {
 					return i;
@@ -15,6 +15,26 @@
 			}
 		};
 		var prot = {};
+		prot.getNextState = function() {
+			var states = this.states;
+			var currState = this.state;
+			var nextStateIndex = indexOfWith(states, 'id', currState.id) + 1;
+			if (nextStateIndex >= states.length) {
+				return null;
+			}
+			var nextState = states[nextStateIndex];
+			return nextState;
+		};
+		prot.getPrevState = function() {
+			var states = this.states;
+			var currState = this.state;
+			var prevStateIndex = indexOfWith(states, 'id', currState.id) - 1;
+			if (prevStateIndex < 0) {
+				return null;
+			}
+			var prevState = states[prevStateIndex];
+			return prevState;
+		};
 		prot.states = [];
 		Object.defineProperty(prot, 'state', {
 			enumerable: true,
@@ -28,30 +48,33 @@
 				this.whenNewState(newState, oldState);
 			}
 		});
+		Object.defineProperty(prot, 'nextState', {
+			enumerable: true,
+			configurable: false,
+			get: function() {
+				return this.getNextState();
+			}
+		});
 		prot.whenNewState = function(newState) {
 			document.getElementById(newState.id).scrollIntoView(true);
 		};
 		prot.next = function() {
-			var states = this.states;
 			var currState = this.state;
-			var nextStateIndex = getIndexByProperty(states, 'id', currState.id) + 1;
-			if (nextStateIndex >= states.length) {
+			var nextState = this.getNextState();
+			if (!nextState) {
 				this.state = currState;
 				return;
 			}
-			var nextState = states[nextStateIndex];
 			this.state = nextState;
 			return this.state;
 		};
 		prot.back = function() {
-			var states = this.states;
 			var currState = this.state;
-			var prevStateIndex = getIndexByProperty(states, 'id', currState.id) - 1;
-			if (prevStateIndex < 0) {
+			var prevState = this.getPrevState();
+			if (!prevState) {
 				this.state = currState;
 				return;
 			}
-			var prevState = states[prevStateIndex];
 			this.state = prevState;
 			return this.state;
 		};
